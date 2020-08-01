bef93850199791f3389188ac047ba26b1ce7cc06
hscho <jhsuper@korea.ac.kr>
2020-08-01 21:39:12 +0900

    Branch test by hscho

--- CIDS.py
+++ CIDS.py
@@ -1,11 +1,11 @@
-##branch test by hscho
-
 from matplotlib import pyplot as plt
 import numpy as np
 import matplotlib
 
-matplotlib.style.use('ggplot')
+#matplotlib.style.use('ggplot')
+#path = "D:\\Idle_4243.asc"
 path = "D:\\Idle_4243.asc"
+
 
 # CAN ID : 45개 (0 ~ 44), 단위 : ms
 CAN_ID = [['42 ', 1000], ['43 ', 1000], ['44 ', 1000]]
